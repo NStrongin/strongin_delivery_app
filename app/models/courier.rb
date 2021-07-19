a71783422afd9dcbaa90d416055714e162cffde3
NStrongin <ston2100@gmail.com>
2021-07-19 23:35:27 +0300

    Create second model

--- app/models/courier.rb
+++ app/models/courier.rb
@@ -1,4 +1,7 @@
 class Courier < ApplicationRecord
+    has_many :packages, dependent: :delete_all
+
     validates :name, presence: true
     validates :email, presence: true, length: { minimum: 10 }
+
 end
